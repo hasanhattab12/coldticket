3ee0c0e9957e1da589bd8d9d9016c365b7015876
hasanhattab12 <45760305+hasanhattab12@users.noreply.github.com>
2019-01-03 21:02:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ client.on("message", (message) => {
  
 });
 
-const developers = ["530438642225643521"]
+const developers = ["526864822348808193"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
